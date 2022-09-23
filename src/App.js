e48ebb4bc3076eb0527394a505b37a404c7c5dc7
Jeremy Love <pristinesolutionsnc@gmail.com>
2022-09-22 21:19:34 -0400

    button implemented

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Button, Stack } from "react-bootstrap"
+import Container from "react-bootstrap/Container"
+
 function App() {
- return "askjeeves"
+ return <Container>
+    <Stack direction="horizontal" gap="2" className="mb-4">
+        <h1 className="me-auto">Budgets</h1>
+        <Button variant="primary">Add Budget</Button>
+        <Button variant="outline-primary">Add Expense</Button>
+    </Stack>
+ </Container>
 }
 
 export default App
