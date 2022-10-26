11233d7db8785fd65add405fff5b7607720d4af3
Jeremy Love <pristinesolutionsnc@gmail.com>
2022-10-25 21:00:35 -0400

    T4B

--- src/contexts/BudgetsContext.js
+++ src/contexts/BudgetsContext.js
@@ -1,5 +1,10 @@
-export function useBudgets() {
+import React, { useContext } from "react"
 
+const BudgetsContext = React.createContext()
+
+
+export function useBudgets() {
+    return useContext(BudgetsContext)
 }
 
 export const BudgetsProvider = ({ children }) => {
