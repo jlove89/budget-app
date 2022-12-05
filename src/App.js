264215997c3e361df1ea2251910a0ea252b425d5
Jeremy Love <pristinesolutionsnc@gmail.com>
2022-12-05 17:02:40 -0500

    T4D

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Container from "react-bootstrap/Container"
 import AddBudgetModal from "./components/AddBudgetModal"
 import AddExpenseModal from "./components/AddExpenseModal"
 import BudgetCard from "./components/BudgetCard"
+import UncategorizedBudgetCard from "./components/UncategorizedBudgetCard"
+import TotalBudgetCard from "./components/TotalBudgetCard"
 import { useBudgets } from "./contexts/BudgetsContext"
 
 function App() {
@@ -45,6 +47,8 @@ function App() {
           />
       )
     })}
+    <UncategorizedBudgetCard />
+    <TotalBudgetCard />
     </div>
  </Container>
  <AddBudgetModal 
